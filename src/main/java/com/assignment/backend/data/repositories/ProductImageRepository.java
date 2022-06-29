49468a6780d399ec885043197a13b78ad9684c3d
tanhao111 <haonguyentan2001@gmail.com>
2022-06-29 14:34:25 +0700

    create product image repository

--- src/main/java/com/assignment/backend/data/repositories/ProductImageRepository.java
+++ src/main/java/com/assignment/backend/data/repositories/ProductImageRepository.java
@@ -1,9 +1,11 @@
 package com.assignment.backend.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.assignment.backend.data.entities.ProductImage;
 
+@Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer>{
     
 }
